512aa5ffd4ed3d8eff044acd7e27ab84806592cd
mbecker20 <becker.maxh@gmail.com>
2025-09-14 12:46:57 -0700

    1.19.4

--- Formula/km.rb
+++ Formula/km.rb
@@ -1,8 +1,8 @@
 class Km < Formula
   desc "Cli to interact with Komodo Core"
   homepage "https://komo.do"
-  url "https://github.com/moghtech/homebrew-komodo/releases/download/v1.19.3/km.tar.gz"
-  sha256 "aad83d86ce2a11d8e17720fcd1047f35067adf6e9bf4bdbace3f7c49031ec5ab"
+  url "https://github.com/moghtech/homebrew-komodo/releases/download/v1.19.4/km.tar.gz"
+  sha256 "81a2a01e11924323287a9675713ae5071011ee28bcb458326c39d917216a5e95"
   license "GPL-V3.0"
 
   def install
--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -1,8 +1,8 @@
 class Periphery < Formula
   desc "Agent to connect with Komodo Core"
   homepage "https://komo.do"
-  url "https://github.com/moghtech/homebrew-komodo/releases/download/v1.19.3/periphery.tar.gz"
-  sha256 "d5cb06dda95a85fe5fb9375638633e9b39ca7bb9c7e07a2fd5302acff7c3e267"
+  url "https://github.com/moghtech/homebrew-komodo/releases/download/v1.19.4/periphery.tar.gz"
+  sha256 "a40565835f5322086bfe36f10507f9ec5aaa501c493f01a0e7acadb124067b07"
   license "GPL-V3.0"
 
   def install
