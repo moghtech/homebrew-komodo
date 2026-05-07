57c23c815f9601038d697f5a5b0f2c784a750bbd
mbecker20 <becker.maxh@gmail.com>
2026-05-07 11:54:09 -0700

    2.2.0

--- Formula/km.rb
+++ Formula/km.rb
@@ -1,13 +1,13 @@
 class Km < Formula
   desc "Cli to interact with Komodo Core"
   homepage "https://komo.do"
-  version "2.1.1"
+  version "2.2.0"
   license "GPL-V3.0"
 
   on_macos do
     on_arm do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/km-apple"
-      sha256 "2348a08d937e04a61b7a2a4ae2fbcdb5c6f6e8380df8a33195ad80e0679aa607"
+      sha256 "0f331f95ae4e1e31a3a77e3b6b6ff89b4eb3e690b0bd84c01fb4a7b34fb04794"
     end
 
     on_intel do
@@ -18,12 +18,12 @@ class Km < Formula
   on_linux do
     on_arm do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/km-aarch64"
-      sha256 "c6d1e5e60e9c1831176baa31f0e6c430cc759066202bf35ab8ccbaeffd7561f0"
+      sha256 "f991b18871253e97df321530f1ac53e7e33b0d7d53e2541feac977a10adf91e6"
     end
 
     on_intel do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/km-x86_64"
-      sha256 "b91d9b75bdabe5447510216ff0a669d5721c43d05ad67d3d14c72567f05a4e93"
+      sha256 "414102fbb259064166702dc7173ffcb1e9acb0707888ffaeba74d5d479a741c5"
     end
   end
 
--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -1,13 +1,13 @@
 class Periphery < Formula
   desc "Agent to connect with Komodo Core"
   homepage "https://komo.do"
-  version "2.1.1"
+  version "2.2.0"
   license "GPL-V3.0"
 
   on_macos do
     on_arm do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/periphery-apple"
-      sha256 "16c56ac365a8d47ac2978eb97662ccf881c07ec800775c6e7582457bd50371a4"
+      sha256 "75a07aaec531b950c8884da8364772f54d8ec9639f4d7223997bf1262fa3cc68"
     end
 
     on_intel do
@@ -18,12 +18,12 @@ class Periphery < Formula
   on_linux do
     on_arm do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/periphery-aarch64"
-      sha256 "549d4a7824da6f49a5aa5dc8c5b4cb3b52562762b7a5a6d63e08ed08cf435aa6"
+      sha256 "ccb253d25eb66ac2c2041192d78875e9c57548637d64abc0030e0f2224239af1"
     end
 
     on_intel do
       url "https://github.com/moghtech/komodo/releases/download/v#{version}/periphery-x86_64"
-      sha256 "e02e125b3b6604926abdd45752453216e597e1c800c83d760e7c66cfc7a50b91"
+      sha256 "ace9007805dbfe75ad73c75c36bb26852fa909d825577f31f5d13eecd3c52660"
     end
   end
 
